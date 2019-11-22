7b38b96b559c70f4abb1567232cce6ff2b1e86a4
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-11-22 21:11:21 +0900

    コンフリクト文削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
-<<<<<<< HEAD
 
-    resources :items, only: [:index]
-    resources :users, only: [:new, :create, :show, :edit]
-=======
   resources :items, only: [:index]
-  resources :users, only: [:new, :create, :show]
->>>>>>> master
+  resources :users, only: [:new, :create, :show, :edit]
+
 end
