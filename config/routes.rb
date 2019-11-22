30dd3d1ea1b9361149f21dfc40e9b20a37667c88
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-11-22 18:41:56 +0900

    ログインページ実装しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "items#index"
 
     resources :items, only: [:index]
-    resources :users, only: [:new, :create, :show]
+    resources :users, only: [:new, :create, :show, :edit]
 end
