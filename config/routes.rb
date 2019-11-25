460d4e796f0e4b4f857afbba7035c976f94ea9d7
yoshida0814 <shin.yoshida.0814@outlook.com>
2019-11-25 16:23:54 +0900

    マイグレーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
 
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
   resources :users, only: [:new, :create, :show, :edit]
 
 end
