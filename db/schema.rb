0180fea11900abb2ae44921174ef725108ffbee1
syooooring <56821781+syooooring@users.noreply.github.com>
2019-11-25 16:25:32 +0900

    deviseインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_25_040248) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
+    t.string "introduction", null: false
+    t.string "family_name", null: false
+    t.string "last_name", null: false
+    t.string "j_family_name", null: false
+    t.string "j_last_name", null: false
+    t.integer "birthday_year", null: false
+    t.integer "birthday_month", null: false
+    t.integer "birthday_day", null: false
+    t.integer "phone_number", null: false
+    t.text "image"
+    t.integer "point", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
