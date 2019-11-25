2a849ea6fe7f8ff07ff3f78930d0a53fee724dc0
techryota <qxnsz54@gmail.com>
2019-11-25 18:53:37 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,21 @@
 
 ActiveRecord::Schema.define(version: 2019_11_25_040248) do
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "image", null: false
+    t.integer "size"
+    t.integer "state", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "shipping_method", null: false
+    t.integer "estimated_shipping_date", null: false
+    t.integer "price", null: false
+    t.text "text", null: false
+    t.integer "inappropriate_item"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,20 +49,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_040248) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-ActiveRecord::Schema.define(version: 2019_11_20_111503) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "image", null: false
-    t.integer "size"
-    t.integer "state", null: false
-    t.integer "delivery_fee", null: false
-    t.integer "shipping_method", null: false
-    t.integer "estimated_shipping_date", null: false
-    t.integer "price", null: false
-    t.text "text", null: false
-    t.integer "inappropriate_item"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
+
 end
