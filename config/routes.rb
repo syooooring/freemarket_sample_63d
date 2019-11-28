f00ec3427fb95b43d7670a7e21b21548cf122d53
hatanoryosuke <r.hatano1986@gmail.com>
2019-11-28 18:38:39 +0900

    商品詳細画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] do
+    collection do
+      get :details
+    end
+  end
   resources :users, only: [:new, :create, :show, :edit]
 
 end
