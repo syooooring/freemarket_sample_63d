af4e7c5b4b2eaaf21b42f8b37767a9f080e305f3
yoshida0814 <shin.yoshida.0814@outlook.com>
2019-11-29 21:07:31 +0900

    マイグレーションファイル　ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_032038) do
+ActiveRecord::Schema.define(version: 2019_11_27_015649) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
