ef6fc108adbe9ac4207b06c7aff9b998a77af9af
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-12-05 21:47:58 +0900

    aタグをlink_toに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,7 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(4)
-    item.update(item_params)   
-    if item.save
+    if item.update(item_params) 
       redirect_to root_path
     else
       redirect_to edit_item_path
