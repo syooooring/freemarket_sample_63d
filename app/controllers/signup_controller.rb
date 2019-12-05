b91cc1121da343acace2b0852b78f55e4a9d32bd
syooooring <56821781+syooooring@users.noreply.github.com>
2019-12-05 10:12:18 +0900

    ユーザー新規２

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -12,12 +12,11 @@ class SignupController < ApplicationController
     rescue ActiveRecord::RecordInvalid => e
       puts e
       puts "保存に失敗しました"
-      # 保存失敗時の処理
     end
   end  
 
   def registration
-    @user = User.new # 新規インスタンス作成
+    @user = User.new
   end
 
   def save_to_session
@@ -51,64 +50,20 @@ class SignupController < ApplicationController
     session[:birthday_year_id] = user_params[:birthday_year_id]
     session[:birthday_month_id] = user_params[:birthday_month_id]
     session[:birthday_day_id] = user_params[:birthday_day_id]
-    @user = User.new # 新規インスタンス作成
-    # session[:nickname] = user_params[:nickname]
-    # session[:email] = user_params[:email]
-    # session[:password] = user_params[:password]
-    # session[:password_confirmation] = user_params[:password_confirmation]
-    # session[:family_name] = user_params[:family_name]
-    # session[:last_name] = user_params[:last_name]
-    # session[:j_family_name] = user_params[:j_family_name]
-    # session[:j_last_name] = user_params[:j_last_name]
-    # session[:birthday_year] = user_params[:birthday_year]
-    # session[:birthday_month] = user_params[:birthday_month]
-    # session[:birthday_day] = user_params[:birthday_day] @user = User.new # 新規インスタンス作成
-    # session[:phone_number] = user_params[:phone_number]
-    # @user = User.new # 新規インスタンス作成
+    @user = User.new
   end
 
   def address
     session[:user_params2] = user_params
     session[:user_params2].merge!(session[:user_params1])
-    @user = User.new # 新規インスタンス作成
+    @user = User.new
     @user.build_address
-    # session[:nickname] = user_params[:nickname]
-    # session[:email] = user_params[:email]
-    # session[:password] = user_params[:password]
-    # session[:family_name] = user_params[:family_name]
-    # session[:last_name] = user_params[:last_name]
-    # session[:j_family_name] = user_params[:j_family_name]
-    # session[:j_last_name] = user_params[:j_last_name]
-    # session[:birthday_year] = user_params[:birthday_year]
-    # session[:birthday_month] = user_params[:birthday_year]
-    # session[:birthday_day] = user_params[:birthday_year]
-
-
-    # session[:phone_number] = user_params[:phone_number]
-    # session[:postal_code] = address_params[:postal_code]
-    # session[:prefectures_id] = address_params[:prefectures_id]
-    # session[:city] = address_params[:city]
-    # session[:banti] = address_params[:banti]
-    # session[:building_name] = address_params[:building_name]
-    # session[:phone_number] = address_params[:phone_number]
-    # @user = User.new # 新規インスタンス作成
   end
 
   def card
     session[:address_params] = user_params[:address_attributes]
-    @user = User.new # 新規インスタンス作成
+    @user = User.new
     @user.build_pay
-    # session[:familyname] = user_params[:familyname]
-    # session[:last_name] = user_params[:last_name]
-    # session[:postal_code] = user_params[:postal_code]
-    # session[:prefectures_id] = user_params[:prefectures_id]
-    # session[:city] = user_params[:city]
-    # session[:banti] = user_params[:banti]
-    # session[:building_name] = user_params[:building_name]
-    # session[:phone_number] = user_params[:phone_number]
-    # session[:card_number] = user_params[:card_number]
-    # session[:card_deadline] = user_params[:card_deadline]
-    # session[:security_code] = user_params[:security_code]
   end
 
   def complete
@@ -152,17 +107,4 @@ class SignupController < ApplicationController
       ]
     )
   end
-
-  # def adderss_params
-  #   params.require(address).permit(
-  #     :familyname, 
-  #     :last_name, 
-  #     :postal_code, 
-  #     :prefectures_id, 
-  #     :city, 
-  #     :banti, 
-  #     :building_name, 
-  #     :phone_number, 
-  #   )
-  # end
 end
