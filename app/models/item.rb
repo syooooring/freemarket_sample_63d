000519e623cd7a2969abcc9b3e44f790c34282d3
yoshida0814 <shin.yoshida.0814@outlook.com>
2019-12-06 14:42:29 +0900

    デプロイ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :name, :price, :image, :text, :prefecture_id, :delivery_id, :delivery_id, :state_id, presence: true#あとで各項目追加する
   validates :prefecture_id,:delivery_id,:delivery_id,:state_id,numericality:{greater_than_or_equal_to:1}
   mount_uploader :image, ImageUploader
