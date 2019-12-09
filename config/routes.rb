8823238fc38665c06de2a4f2093e0c6a0f28ec07
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-12-09 12:57:17 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all,
+  devise_for :users, skip: :new,
   controllers: {
     sessions: 'users/sessions',
     registrations: "users/registrations",
