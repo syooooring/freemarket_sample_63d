5a8a5852df8595df92fb7d47a7ce67b53b46392e
syooooring <56821781+syooooring@users.noreply.github.com>
2019-12-09 16:16:41 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all,
+  devise_for :users, skip: :new,
   controllers: {
-    sessions: 'users/sessions',
     registrations: "users/registrations",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
 
-  devise_scope :user do
-    delete 'destroy' => 'devise/sessions#destroy',as: :current_user_destroy
-  end
+  # devise_scope :user do
+  #   delete 'destroy' => 'devise/sessions#destroy',as: :current_user_destroy
+  # end
 
   root to: "items#index"
 
