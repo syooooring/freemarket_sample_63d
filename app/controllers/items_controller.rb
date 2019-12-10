eff2746a5de2847e8288506151fb4920b48f6fc0
techryota <qxnsz54@gmail.com>
2019-12-10 15:09:48 +0900

    item-details-function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :set_item, only: [:edit, :update]
+
   def index
     @items = Item.all.limit(10).order(id: "DESC")
   end
@@ -8,32 +8,61 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.thumbnails.build
   end
 
   def create
+    @item = Item.new(item_params)
+    respond_to do |format|
+      if @item.save
+          params[:thumbnails][:images].each do |image|
+            @item.thumbnails.create(images: image, item_id: @item.id)
+          end
+        format.html{redirect_to root_path}
+      else
+        @item.thumbnails.build
+        format.html{render action: 'new'}
+      end
+    end
   end
 
   def edit
+    @item = Item.find(1)
   end
 
   def update
+    item = Item.find(1)
+    if item.update(item_params) 
+      redirect_to root_path
+    else
+      redirect_to edit_item_path
+    end
   end
 
   def details
+    @item = Item.find(params[:id])
+    @items = Item.where(saler_id: @item.saler_id).limit(6).order('id DESC')
+    @price = "¥#{@item.price.to_s(:delimited)}"
+    @state = @item.state.name
+    @delivery = @item.delivery.name
+    @date = @item.estimated_shipping_date.name
+    # @shipping_method = @item.shipping_method.name
   end
   
+  def address
+  end
+
   def buy
   end
 
   def buy1
   end
 
-  private
+private
+
   def item_params
-    params.require(:item).permit(:name)
+    params.require(:item).permit(:name, :size, :state_id, :delivery_id, :shipping_method_id, :estimated_shipping_date_id, :price, :text, :prefecture_id,  thumbnails_attributes: [:images]).merge(user_id: current_user.id, saler_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,52 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all
-  devise_scope :user do
-    delete 'destroy' => 'devise/sessions#destroy',as: :current_user_destroy
-  end
+  devise_for :users, skip: :new,
+  controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+
+  # devise_scope :user do
+  #   delete 'destroy' => 'devise/sessions#destroy',as: :current_user_destroy
+  # end
+
   root to: "items#index"
+
+
+  resources :sign_ups, only: [:index] do
+    collection do
+      get :registration  # 登録情報の一部を入力してもらう
+      post :registration, to: 'signups#step1_validates'  #step1で入力したデータのバリデーションチャックをする(詳しくはコントローラーで説明) render後にリロードしてもエラーが出ないようにget'step1'とURLは同じに設定する
+      get :sms_confirmation  # 残りの登録情報を入力してもらう
+      post :sms_confirmation, to: 'signups#create' # 入力した情報すべてを保存 こっちもrenderするなら揃える
+    end
+  end
 
   resources :items, only: [:index, :show, :new, :create, :edit, :update] do
     collection do
       get :buy
       get :buy1
+    end
+    member do
       get :details
+      get :address
     end
   end    
   resources :pays, only: [:new]
   resources :users, only: [:new, :create, :show, :edit] do
     member do
-      get  :identification
+      get :identification
       get :logout
-      get :sell
+      get :selling
+      get :saling
+      get :sold
     end
   end
   resources :signup, only: [:create] do
     collection do
       get :registration
       get :sms_confirmation
+      post :sms_confirmation
       get :address
       post :address
       get :card
@@ -37,6 +60,9 @@ Rails.application.routes.draw do
       get :profile
       get :card_info
       get :zoom
+      get :saling
+      get :selling
+      get :sold
     end
   end
 end
