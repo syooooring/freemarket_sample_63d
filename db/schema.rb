c8a3bf6593fcaf575ddf98e850c3631f889a4fb6
syooooring <56821781+syooooring@users.noreply.github.com>
2019-12-10 16:46:51 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -27,13 +27,6 @@ ActiveRecord::Schema.define(version: 2019_12_04_084428) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "category_name", null: false
-    t.integer "parent_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "size"
