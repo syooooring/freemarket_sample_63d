b1ba054b07ab24d14cb7ffcb60b7c6d112a0d783
yoshida0814 <shin.yoshida.0814@outlook.com>
2019-12-12 20:07:22 +0900

    fix

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -14,21 +14,18 @@ class CardsController < ApplicationController
   # 登録画面で入力した情報をDBに保存
   def create
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    if params['payjp-token'].blank?
-      render 'cards/index'
-    else
-      customer = Payjp::Customer.create( # ここで先ほど生成したトークンを顧客情報と紐付け、PAY.JP管理サイトに送信
-        email: current_user.email,
-        card: params['payjp-token'],
-        metadata: {user_id: current_user.id} # 記述しなくても大丈夫です
-      )
+    # if params['payjp-token'].blank?
+    #   render 'cards/index'
+    # else
+      customer = Payjp::Customer.create(card: params['payjp-token'])
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+    
       if @card.save
         redirect_to cards_path
       else
         redirect_to route_patu
       end
-    end
+    # end
   end
 
   # 後ほど削除機能を実装します。
